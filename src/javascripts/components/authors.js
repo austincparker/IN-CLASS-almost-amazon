8721b58ab323f20b74507d7d5b162bc31e1075a3
Austin Parker <austincparker@gmail.com>
2021-08-24 16:33:51 -0500

    fixed the authors bug

--- src/javascripts/components/authors.js
+++ src/javascripts/components/authors.js
@@ -6,7 +6,15 @@ const showAuthors = (array) => {
 
   array.forEach((item) => {
     // FIXME: STUDENTS create cards for your authors
-    document.querySelector('#store').innerHTML += `${item}`;
+    document.querySelector('#store').innerHTML += `<div class="card" style="width: 18rem;">
+    <div class="card-body">
+      <h5 class="card-title">${item.first_name} ${item.last_name}</h5>
+      <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <a href="#" class="card-link">Card link</a>
+      <a href="#" class="card-link">Another link</a>
+    </div>
+  </div>`;
   });
 };
 
