1e04628a74095bce1a7828d7d5a57e9511677133
Austin Parker <austincparker@gmail.com>
2021-08-27 15:36:55 -0500

    starting on show authors

--- src/javascripts/helpers/data/authorData.js
+++ src/javascripts/helpers/data/authorData.js
@@ -11,7 +11,17 @@ const getAuthors = () => new Promise((resolve, reject) => {
 });
 // DELETE AUTHOR
 // CREATE AUTHOR
+
+const createAuthor = (authorObj) => new Promise((resolve, reject) => {
+  axios.post(`${dbUrl}/authors.json`, authorObj)
+    .then((response) => {
+      const body = { firebaseKey: response.data.name }
+
+      axios.patch(`${dbUrl}/authors/${response.data.name}.json`, body)
+        .then(() => console.warn("author created"));
+    }).catch((error) => reject(error));
+});
 // UPDATE AUTHOR
 // SEARCH AUTHORS
 
-export default getAuthors;
+export default { getAuthors, createAuthor };
